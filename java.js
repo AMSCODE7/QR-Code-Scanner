d0346fb4cc19c4b1fb3593bf8883ba38fc187b7f
Amit Kumar Singh <amscode7@gmail.com>
2022-07-11 13:59:52 +0530

    Complete QR code Scanner.

--- java.js
+++ java.js
@@ -1,21 +1,38 @@
 const wrapper  = document.querySelector(".wrapper"),
 form  = wrapper.querySelector("form"),
-fileInp = form.querySelector("input");
+fileInp = form.querySelector("input"),
+infoText = form.querySelector("p"),
+closeBtn = wrapper.querySelector(".close"),
+copyBtn = wrapper.querySelector(".copy");
 
-function fetchRequest(formData) {
+function fetchRequest(formData , file) {
+    infoText.innerText = "Scanning Qr Code";
     fetch("http://api.qrserver.com/v1/read-qr-code/" , {
         method: "POST" , body: formData 
     }).then(res => res.json()).then(result => {
+        result = result[0].symbol[0].data;
+        infoText.innerText = result ? "upload Qr Code for Scanner" : "Could not Scan Qr Code";
+        if(!file)return;
+        wrapper.querySelector("textarea").innerText = result;
+        form.querySelector("img").src = URL.createObjectURL(file);
         wrapper.classList.add("active");
-        console.log(result);
+    }).catch(() => {
+        infoText.innerText = "Could not Scan Qr Code";
     });
 }
 
 fileInp.addEventListener("change" , e => {
     let file = e.target.files[0];
+    if(!file)return;
     let formData = new FormData();
     formData.append("file",file);
-    fetchRequest(formData);
+    fetchRequest(formData , file);
+});
+
+copyBtn.addEventListener("click", () => {
+    let text = wrapper.querySelector("textarea").textContent;
+    navigator.clipboard.writeText(text);
 })
 
 form.addEventListener("click" , () => fileInp.click());
+closeBtn.addEventListener("click" ,() => wrapper.classList.remove("active"))
